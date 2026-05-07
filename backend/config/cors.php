de5f5d159d77c494de986aefc9944d91adee470e
Tolentino-KristineMae <maemaemaetolentino@gmail.com>
2026-05-07 16:31:37 +0800

    test3

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://localhost:3000',
+        env('FRONTEND_URL', '*'),
+    ],
 
     'allowed_origins_patterns' => [],
 
